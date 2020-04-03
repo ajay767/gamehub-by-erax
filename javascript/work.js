715f1359314497646d60fce3acd27270971ccaf2
AJAY YADAV <58135870+ajay767@users.noreply.github.com>
2020-04-03 21:40:43 +0530

    updated work file

--- javascript/work.js
+++ javascript/work.js
@@ -1,4 +1,10 @@
-alert("This game is developed to shortout regular Family matters, Hope you will love it! BY AJU YADUVANSHI");
+if( confirm("want to play game") )
+{
+
+
+
+alert("This game is developed to shortout regular Family matters, Hope you will love it! BY AJU YADUVANSHI")
+
 var player1=prompt("Input first player name");
 var player2=prompt("Input second player name");
 document.querySelector(".player_1 h4").innerHTML=player1;
@@ -34,3 +40,50 @@ document.querySelector(".result h3").innerHTML=  winner + " is the winner!"
 
 
 document.querySelector(".reload button").addEventListener("click",through);
+
+}
+else
+{
+
+
+
+
+
+
+
+
+for (let i = 0; i < 5; i++) {
+
+
+    document.querySelectorAll(".tune")[i].addEventListener("click", function () {
+        var innerbutton = this.innerHTML;
+        switch (innerbutton) {
+            case "W":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "S":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "X":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "E":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            case "Z":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+
+            default:
+                alert("press right key");
+
+        }
+
+    });
+}
+};
